6241eb031d9c515babd6a0760b650d71ea8a54bf
aluiz21 <aluiz@valcann.com.br>
2024-07-07 18:20:32 -0300

    Add files via upload

---
+++ build/precache-manifest.dcfafd4514e9025f51cd01b33505ae78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca2d0804088ec806237c02bb717bc56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "177cc64f6745baa406ed",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "9f6fdf7402e73b120f00",
+    "url": "/static/js/2.67f2198d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67f2198d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "177cc64f6745baa406ed",
+    "url": "/static/js/main.cf5531df.chunk.js"
+  },
+  {
+    "revision": "67c46fa5191a7506d6bc",
+    "url": "/static/js/runtime-main.bbba985a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
